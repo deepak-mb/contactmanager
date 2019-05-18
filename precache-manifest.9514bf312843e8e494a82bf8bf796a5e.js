0c9a6e7e396159781c6e26ad1d75e8c141219cc9
Deepak <deepak25mb@gmail.com>
2019-05-18 18:50:14 +0530

    Updates

---
+++ precache-manifest.9514bf312843e8e494a82bf8bf796a5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec94ece89641e7e61f9e326ab58d37a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "93377b1cba7b27c247f9",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ca33e744d4273d5f67ba",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "93377b1cba7b27c247f9",
+    "url": "/contactmanager/static/js/2.52941224.chunk.js"
+  },
+  {
+    "revision": "ca33e744d4273d5f67ba",
+    "url": "/contactmanager/static/js/main.a48c7975.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
